ea2b797ff25aeb54c6e1705c2c5b128b96f21dfe
Jan Vargovský <jan.vargovsky@gmail.com>
2019-12-02 21:00:23 +0100

    Add 2019 Day 1 Part 2

--- 2019/1/1.py
+++ 2019/1/1.py
@@ -1,10 +1,14 @@
 def calculate_fuel(mass):
-    return mass // 3 - 2
+    sum = 0
+    while mass > 0:
+        mass = max(0, mass // 3 - 2)
+        sum += mass
+    return sum
 
 assert(calculate_fuel(12) == 2)
 assert(calculate_fuel(14) == 2)
-assert(calculate_fuel(1969) == 654)
-assert(calculate_fuel(100756) == 33583)
+assert(calculate_fuel(1969) == 966)
+assert(calculate_fuel(100756) == 50346)
 
 with open('input.txt') as f:
     print(sum(map(lambda l: calculate_fuel(int(l)), f.readlines())))
