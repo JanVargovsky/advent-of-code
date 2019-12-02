daf33fa2a035b27fcf70b15ee5ef3c846e28daa2
Jan Vargovský <jan.vargovsky@gmail.com>
2019-12-02 21:20:00 +0100

    Add 2019 Day 2 Part 2

--- 2019/2/2.py
+++ 2019/2/2.py
@@ -1,15 +1,15 @@
 def compute(memory):
-    i = 0
-    while memory[i] != 99:
-        if memory[i] == 1:
+    ip = 0
+    while memory[ip] != 99:
+        if memory[ip] == 1:
             # add 1, 2 = positions, 3 = result position
-            memory[memory[i + 3]] = memory[memory[i + 1]] + memory[memory[i + 2]]
-            i += 4
-        elif memory[i] == 2:
-            memory[memory[i + 3]] = memory[memory[i + 1]] * memory[memory[i + 2]]
-            i += 4
+            memory[memory[ip + 3]] = memory[memory[ip + 1]] + memory[memory[ip + 2]]
+            ip += 4
+        elif memory[ip] == 2:
+            memory[memory[ip + 3]] = memory[memory[ip + 1]] * memory[memory[ip + 2]]
+            ip += 4
         else:
-            i += 1
+            ip += 1
     return memory
 
 assert(compute([1,9,10,3,2,3,11,0,99,30,40,50]) == [3500,9,10,70,2,3,11,0,99,30,40,50])
@@ -19,7 +19,12 @@ assert(compute([2,4,4,5,99,0]) == [2,4,4,5,99,9801])
 assert(compute([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99])
 
 with open('input.txt') as f:
-    program = [int(i) for i in f.readline().split(',')]
-    program[1] = 12
-    program[2] = 2
-    print(compute(program)[0])
+    program_original = [int(i) for i in f.readline().split(',')]
+
+for noun in range(0, 100):
+    for verb in range(0, 100):
+        program = program_original.copy()
+        program[1] = noun
+        program[2] = verb
+        if compute(program)[0] == 19690720:
+            print(100 * noun + verb)
