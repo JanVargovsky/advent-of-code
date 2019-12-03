bce39c35b1bc0e9cf688689d88d6eda607413640
Jan Vargovský <jan.vargovsky@gmail.com>
2019-12-03 12:21:43 +0100

    Add 2019 Day 3 Part 2

--- 2019/3/3.py
+++ 2019/3/3.py
@@ -15,7 +15,7 @@ def parse_input(text):
 assert(parse_input('R75,D30,R83') == [Move('R', 75), Move('D', 30), Move('R', 83)])
 
 def build(moves):
-    points = set()
+    points = {}
     move_to_vec = {
         'R': Point(1, 0),
         'U': Point(0, 1),
@@ -24,29 +24,31 @@ def build(moves):
     }
 
     p = Point(0, 0)
+    d = 0
     for m in moves:
         vec = move_to_vec[m.direction]
-        for i in range(m.length):
-            points.add(p)
+        for _ in range(m.length):
+            points[p] = d
             p = Point(p.x + vec.x, p.y + vec.y)
+            d += 1
     return points
 
 def solve(w1, w2):
     p1 = build(w1)
     p2 = build(w2)
-    p12 = p1.intersection(p2)
+    p12 = p1.keys() & p2.keys()
     center = Point(0, 0)
     p12.remove(center)
     m = None
     for x in p12:
-        if m is None or manhattan_distance(x, center) < manhattan_distance(m, center):
+        if m is None or p1[x] + p2[x] < p1[m] + p2[m]:
             m = x
-    d = manhattan_distance(m, center)
-    return d
+    steps = p1[m] + p2[m]
+    return steps
 
-assert(solve(parse_input("R8,U5,L5,D3"), parse_input("U7,R6,D4,L4")) == 6)
-assert(solve(parse_input("R75,D30,R83,U83,L12,D49,R71,U7,L72"), parse_input("U62,R66,U55,R34,D71,R55,D58,R83")) == 159)
-assert(solve(parse_input("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"), parse_input("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")) == 135)
+assert(solve(parse_input("R8,U5,L5,D3"), parse_input("U7,R6,D4,L4")) == 30)
+assert(solve(parse_input("R75,D30,R83,U83,L12,D49,R71,U7,L72"), parse_input("U62,R66,U55,R34,D71,R55,D58,R83")) == 610)
+assert(solve(parse_input("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"), parse_input("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")) == 410)
 
 with open('input.txt') as f:
     wire1 = parse_input(f.readline())
