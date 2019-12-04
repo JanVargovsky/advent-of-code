dcabcfaae473f8488f421421875810070198783c
Jan Vargovský <jan.vargovsky@gmail.com>
2019-12-04 11:45:09 +0100

    Add 2019 Day 4 Part 2

--- 2019/4/4.py
+++ 2019/4/4.py
@@ -1,18 +1,30 @@
-count = 0
-for i in range (172930, 683082):
+def password(i):
     s = str(i)
-    if int(s[0]) > int(s[1]) \
-        or int(s[1]) > int(s[2]) \
-        or int(s[2]) > int(s[3]) \
-        or int(s[3]) > int(s[4]) \
-        or int(s[4]) > int(s[5]):
-        continue
-    adjacent = False
-    for a in range(5):
-        if s[a] is s[a+1]:
-            adjacent = True
-            break
-    if not adjacent:
-        continue
-    count += 1
+    for i in range(5):
+        if s[i] > s[i+1]:
+            return False
+    
+    i = 0
+    while i < 5:
+        start = i
+        while i < 5 and s[i] == s[i + 1]:
+            i += 1
+        if (i - start + 1) == 2:
+            return True
+        i += 1
+
+    return False
+
+# assert(password("111111") == True)
+assert(password("223450") == False)
+assert(password("123789") == False)
+
+assert(password("112233") == True)
+assert(password("123444") == False)
+assert(password("111122") == True)
+
+count = 0
+for i in range(172930, 683082):
+    if password(i):
+        count += 1
 print(count)
