4b2e5b27d2de14f8bb1e366b6504e198db6b2ded
Jan Vargovský <jan.vargovsky@gmail.com>
2019-12-05 18:23:30 +0100

    Add 2018 Day 1 Part 2

--- 2018/1/1.py
+++ 2018/1/1.py
@@ -1,2 +1,12 @@
 with open('input.txt') as f:
-    print(sum([int(i) for i in f.readlines()]))
+    l = [int(i) for i in f.readlines()]
+f = 0
+fs = set()
+fs.add(f)
+
+while True:
+    for i in l:
+        f += i
+        if f in fs:
+            print(f)
+        fs.add(f)
