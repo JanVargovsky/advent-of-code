aaf080b6fe58ccc2060f861e538f6efc07c71d0a
Jan Vargovský <jan.vargovsky@gmail.com>
2019-12-14 21:21:36 +0100

    Add 2019 Day 13

---
+++ 2019/13/13.py
@@ -0,0 +1,100 @@
+from queue import Queue
+import itertools
+
+opcodes = {
+    # Opcode: arity
+    99: 0,  # halt
+    1: 3,  # add
+    2: 3,  # multiply
+    3: 1,  # input
+    4: 1,  # output
+    5: 2,  # jump if true
+    6: 2,  # jump if false
+    7: 3,  # less than
+    8: 3,  # equals
+    9: 1,  # relative base offset
+}
+
+
+def get_mode(opcode, p):
+    return opcode // 10**(2 + p) % 10
+
+
+assert(get_mode(1002, 0) == 0)
+assert(get_mode(1002, 1) == 1)
+assert(get_mode(1002, 2) == 0)
+
+
+def load_parameters(memory, ip, raw, opcode, offset):
+    values = []
+    pointers = []
+    for i in range(opcodes[opcode]):
+        mode = get_mode(raw, i)
+        if mode == 0:  # position mode
+            values.append(memory[memory[ip + i]])
+        elif mode == 1:  # immediate mode
+            values.append(memory[ip + i])
+        elif mode == 2:  # relative mode
+            values.append(memory[offset + memory[ip + i]])
+        pointers.append(
+            offset + memory[ip + i] if mode == 2 else memory[ip + i])
+    return values, pointers
+
+
+def run(memory, input):
+    memory += [0] * 10000
+    ip = 0
+    offset = 0
+    while True:
+        raw = memory[ip]
+        opcode = raw % 100
+        if opcode == 99:
+            break
+        ip += 1
+        values, pointers = load_parameters(memory, ip, raw, opcode, offset)
+        ip += opcodes[opcode]
+
+        if opcode == 1:
+            memory[pointers[2]] = values[0] + values[1]
+        elif opcode == 2:
+            memory[pointers[2]] = values[0] * values[1]
+        elif opcode == 3:
+            memory[pointers[0]] = input()
+        elif opcode == 4:
+            # print(values[0])
+            yield values[0]
+        elif opcode == 5:
+            if values[0] != 0:
+                ip = values[1]
+        elif opcode == 6:
+            if values[0] == 0:
+                ip = values[1]
+        elif opcode == 7:
+            memory[pointers[2]] = 1 if values[0] < values[1] else 0
+        elif opcode == 8:
+            memory[pointers[2]] = 1 if values[0] == values[1] else 0
+        elif opcode == 9:
+            offset += values[0]
+        else:
+            raise Exception()
+
+
+with open('input.txt') as f:
+    program = [int(i) for i in f.readline().split(',')]
+
+
+EMPTY = 0
+WALL = 1
+BLOCK = 2
+PADDLE = 3
+BALL = 4
+
+
+game = {}
+output = run(program, None)
+for x in output:
+    y = next(output)
+    id = next(output)
+    game[(x, y)] = id
+
+print(list(game.values()).count(BLOCK))
