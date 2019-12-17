bcd8817731eb45a18ca86e816e283b3581b7d102
Jan Vargovský <jan.vargovsky@gmail.com>
2019-12-17 14:59:14 +0100

    Add 2019 Day 16 Part 2

--- 2019/16/16.py
+++ 2019/16/16.py
@@ -1,3 +1,6 @@
+from functools import reduce
+from itertools import accumulate, cycle, islice
+
 with open('input.txt') as f:
     sequence = [int(d) for d in f.readline().rstrip()]
 
@@ -22,16 +25,21 @@ assert(apply_phase([1, 2, 3, 4, 5, 6, 7, 8]) == [4, 8, 2, 2, 6, 1, 5, 8])
 
 
 def solve(sequence, phases):
+    offset = reduce(lambda acc, d: acc * 10 + d, sequence[:7])
+    length = len(sequence) * 10000 - offset
+    sequence = islice(cycle(reversed(sequence)), length)
+
     for phase in range(phases):
-        sequence = apply_phase(sequence)
-    return sequence[:8]
+        sequence = [d % 10 for d in accumulate(sequence)]
+
+    return sequence[-8:][::-1]
 
 
-assert(solve([1, 2, 3, 4, 5, 6, 7, 8], 2) == [3, 4, 0, 4, 0, 4, 3, 8])
-assert(solve([1, 2, 3, 4, 5, 6, 7, 8], 3) == [0, 3, 4, 1, 5, 5, 1, 8])
-assert(solve([1, 2, 3, 4, 5, 6, 7, 8], 4) == [0, 1, 0, 2, 9, 4, 9, 8])
-assert(solve([8, 0, 8, 7, 1, 2, 2, 4, 5, 8, 5, 9, 1, 4, 5, 4, 6, 6, 1, 9,
-              0, 8, 3, 2, 1, 8, 6, 4, 5, 5, 9, 5], 100) == [2, 4, 1, 7, 6, 1, 7, 6])
-
+assert(solve([0, 3, 0, 3, 6, 7, 3, 2, 5, 7, 7, 2, 1, 2, 9, 4, 4, 0, 6, 3,
+              4, 9, 1, 5, 6, 5, 4, 7, 4, 6, 6, 4], 100) == [8, 4, 4, 6, 2, 0, 2, 6])
+assert(solve([0, 2, 9, 3, 5, 1, 0, 9, 6, 9, 9, 9, 4, 0, 8, 0, 7, 4, 0, 7,
+              5, 8, 5, 4, 4, 7, 0, 3, 4, 3, 2, 3], 100) == [7, 8, 7, 2, 5, 2, 7, 0])
+assert(solve([0, 3, 0, 8, 1, 7, 7, 0, 8, 8, 4, 9, 2, 1, 9, 5, 9, 7, 3, 1,
+              1, 6, 5, 4, 4, 6, 8, 5, 0, 5, 1, 7], 100) == [5, 3, 5, 5, 3, 7, 3, 1])
 
 print(*(solve(sequence, 100)), sep="")
