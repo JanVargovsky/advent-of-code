523207245fbf5785fc644587f2b19a41d919d021
Jan Vargovský <jan.vargovsky@gmail.com>
2019-12-19 09:50:21 +0100

    Add 2019 Day 17 Part 2

--- 2019/17/17.py
+++ 2019/17/17.py
@@ -1,4 +1,6 @@
 from collections import namedtuple
+from queue import Queue
+import re
 
 opcodes = {
     # Opcode: arity
@@ -85,67 +87,169 @@ with open('input.txt') as f:
 Int2 = namedtuple("Int2", ["x", "y"])
 
 
-def find_robot(map):
-    for y in range(len(map)):
-        for x in range(len(map[y])):
-            if map[y][x] in '^v<>':
+def find_robot(camera):
+    for y in range(len(camera)):
+        for x in range(len(camera[y])):
+            if camera[y][x] in '^v<>':
                 return Int2(x, y)
 
 
-def is_valid_position(map, position):
-    return position.x >= 0 and position.y >= 0 and position.y < len(map) and position.x < len(map[position.y])
+def is_valid_position(camera, position):
+    return position.x >= 0 and position.y >= 0 and position.y < len(camera) and position.x < len(camera[position.y])
 
 
-def find_direction(map, robot, directions, visited):
-    for direction in directions:
+def find_direction(camera, robot, directions, visited):
+    for i, direction in enumerate(directions):
         position = Int2(robot.x + direction.x, robot.y + direction.y)
-        if is_valid_position(map, position) and \
-                map[position.y][position.x] == "#" and \
+        if is_valid_position(camera, position) and \
+                camera[position.y][position.x] == "#" and \
                 not position in visited:
-            return direction
+            return i, direction
 
-    return None
+    return None, None
 
 
-def solve(map):
-    robot = find_robot(map)
+def find_path(camera):
+    robot = find_robot(camera)
     directions = [
-        Int2(1, 0),
-        Int2(0, 1),
-        Int2(-1, 0),
-        Int2(0, -1)
+        Int2(1, 0),  # >
+        Int2(0, 1),  # v
+        Int2(-1, 0),  # <
+        Int2(0, -1)  # ^
     ]
     visited = set([robot])
-    intersections = []
+    direction_index = ">v<^".index(camera[robot.y][robot.x])
+    path = []
 
     while True:
-        direction = find_direction(map, robot, directions, visited)
+        new_direction_index, direction = find_direction(
+            camera, robot, directions, visited)
         if not direction:
             break
 
+        if directions[direction_index] == directions[new_direction_index - 1]:
+            path.append("R")
+        elif directions[direction_index - 1] == directions[new_direction_index]:
+            path.append("L")
+        direction_index = new_direction_index
+
+        i = 0
         while True:
             new_robot = Int2(robot.x + direction.x, robot.y + direction.y)
-            if not is_valid_position(map, new_robot) or \
-                    map[new_robot.y][new_robot.x] != "#":
+            if not is_valid_position(camera, new_robot) or \
+                    camera[new_robot.y][new_robot.x] != "#":
+                path.append(str(i))
                 break
-
+            i += 1
+            # path.append("F")
             robot = new_robot
-            if robot in visited:
-                intersections.append(robot)
-            else:
-                visited.add(robot)
+            visited.add(robot)
 
-    return sum(p.x * p.y for p in intersections)
+    return "".join(path)
 
 
-assert(solve("""..#..........
-..#..........
-#######...###
-#.#...#...#.#
-#############
-..#...#...#..
-..#####...^..""".splitlines()) == 76)
+assert(find_path("""#######...#####
+#.....#...#...#
+#.....#...#...#
+......#...#...#
+......#...###.#
+......#.....#.#
+^########...#.#
+......#.#...#.#
+......#########
+........#...#..
+....#########..
+....#...#......
+....#...#......
+....#...#......
+....#####......""".splitlines()) == "R,8,R,8,R,4,R,4,R,8,L,6,L,2,R,4,R,4,R,8,R,8,R,8,L,6,L,2".replace(",", ""))
 
 
-map = "".join(map(chr, run(program, None))).splitlines()
-print(solve(map))
+def compress(text):
+    for a in range(4, 20):
+        for b in range(4, 20):
+            for c in range(4, 20):
+                ra = text[:a]
+                compressed = text.replace(ra, "")
+                rb = compressed[:b]
+                compressed = compressed.replace(rb, "")
+                rc = compressed[:c]
+                compressed = compressed.replace(rc, "")
+
+                if len(compressed) == 0:
+                    compressed = text.replace(ra, "A").replace(
+                        rb, "B").replace(rc, "C")
+                    if all(c in "ABC" for c in compressed):
+                        return compressed, ra, rb, rc
+
+    raise Exception()
+
+
+assert(compress('L4R8L6L10L6R8R10L6L6L4R8L6L10L6R8R10L6L6L4L4L10L4L4L10L6R8R10L6L6L4R8L6L10L6R8R10L6L6L4L4L10') ==
+       ("ABABCCBABC", "L4R8L6L10", "L6R8R10L6L6", "L4L4L10"))
+
+
+def to_commands(command):
+    return ",".join(re.split("(?<=\\d)(?=\\D)|(?=\\d)(?<=\\D)", command))
+
+
+assert(to_commands("L1R2L10R10") == "L,1,R,2,L,10,R,10")
+
+
+def readlines(it):
+    result = []
+    for c in map(chr, it):
+        if c == "\n":
+            line = "".join(result)
+            print("output:", line)
+            yield line
+            result = []
+        else:
+            result.append(c)
+    if result:
+        line = "".join(result)
+        print("output:", line)
+        yield line
+
+
+def send(data):
+    global input
+    print("input:", data)
+    if data is int:
+        input.put(data)
+    else:
+        for d in data:
+            input.put(ord(d))
+    input.put(ord("\n"))
+
+
+program[0] = 2
+camera = []
+input = Queue()
+intcode = readlines(run(program, input.get))
+while True:
+    line = next(intcode)
+    if line == "":
+        break
+    camera.append(line)
+
+path = find_path(camera)
+print("path:", path)
+main, function_a, function_b, function_c = compress(path)
+main = ",".join(main)
+function_a = to_commands(function_a)
+function_b = to_commands(function_b)
+function_c = to_commands(function_c)
+assert(next(intcode) == "Main:")
+send(main)
+assert(next(intcode) == "Function A:")
+send(function_a)
+assert(next(intcode) == "Function B:")
+send(function_b)
+assert(next(intcode) == "Function C:")
+send(function_c)
+assert(next(intcode) == "Continuous video feed?")
+send("n")
+
+*_, dust = intcode
+print("dust:", ord(dust))
