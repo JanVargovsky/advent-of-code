62d53ee2e747ebb22012dc27827971550cd4dc21
Jan Vargovský <jan.vargovsky@gmail.com>
2019-12-20 13:02:12 +0100

    Add 2019 Day 18 Part 2

--- 2019/18/18.py
+++ 2019/18/18.py
@@ -4,10 +4,10 @@ from queue import Queue
 Int2 = namedtuple("Int2", ["x", "y"])
 
 
-def find_entrance(m):
+def find(m, c):
     for y in range(len(m)):
         for x in range(len(m[y])):
-            if m[y][x] == "@":
+            if m[y][x] == c:
                 return Int2(x, y)
 
 
@@ -48,16 +48,18 @@ def find_path(m, start, end):
     q = Queue()
     q.put([start])
 
-    while q:
+    while not q.empty():
         *path, current = q.get()
 
-        if current in visited or m[current.y][current.x] == "#":
+        if current in visited:
+            continue
+        visited.add(current)
+
+        if m[current.y][current.x] == "#":
             continue
 
         if current == end:
             return path + [current]
-
-        visited.add(current)
 
         for direction in directions:
             q.put(path +
@@ -73,78 +75,96 @@ assert(find_path("""#########
 
 def solve(m):
     all_keys = {v: p for p, v in enumerate2d(m) if is_key(v)}
-    key_distances = {key: {} for key in all_keys.keys()}
-    key_distances["@"] = {}
-    for a, pa in {"@": find_entrance(m), **all_keys}.items():
+    robots = {v: find(m, v) for v in "1234"}
+    key_distances = {key: {} for key in ([*all_keys] + [*robots])}
+    for a, pa in {**robots, **all_keys}.items():
         for b, pb in all_keys.items():
             if a == b or b in key_distances[a]:
                 continue
             path = find_path(m, pa, pb)
+            if not path:
+                continue
             key_distances[a][b] = key_distances[b][a] = {
                 # "path": path,
                 "distance": len(path) - 1,
                 "doors": frozenset(m[y][x] for x, y in path if is_door(m[y][x])),
                 "keys": frozenset(m[y][x] for x, y in path if is_key(m[y][x])),
-                "required_keys": frozenset(m[y][x].lower() for x, y in path if is_door(m[y][x]))
+                "required_keys": frozenset(m[y][x].lower() for x, y in path[1:-1] if is_door(m[y][x]))
             }
 
-    states = {("@", frozenset("@")): 0}  # position, keys: distance
+    states = {(*robots.keys(), frozenset()): 0}  # positions, keys: distance
     best_distance = 1e10
     while states:
         new_states = {}
-        for (position, keys), d in states.items():
+        for (*positions, keys), d in states.items():
             if not all_keys.keys() - keys:
                 best_distance = min(best_distance, d)
                 continue
 
-            for key, key_data in key_distances[position].items():
-                if key in keys:  # already obtained key
-                    continue
-                if key_data["required_keys"] - keys:  # missing key
-                    continue
+            for i, position in enumerate(positions):
+                for key, key_data in key_distances[position].items():
+                    if key in keys:  # already obtained key
+                        continue
+                    if key_data["required_keys"] - keys:  # missing key
+                        continue
 
-                new_keys = keys \
-                    .union(key) \
-                    .union(key_data["keys"])
-                new_distance = d + key_data["distance"]
-                new_states[(key, new_keys)] = min(
-                    new_states.get((key, new_keys), 1e10), new_distance)
+                    new_keys = keys \
+                        .union(key) \
+                        .union(key_data["keys"])
+
+                    new_distance = d + key_data["distance"]
+
+                    new_positions = positions.copy()
+                    new_positions[i] = key
+                    new_positions = tuple((*new_positions, new_keys))
+                    new_states[new_positions] = min(
+                        new_states.get(new_positions, 1e10), new_distance)
 
         states = new_states
     return best_distance
 
 
-assert(solve("""#########
-#b.A.@.a#
-#########""".splitlines()) == 8)
-assert(solve("""########################
-#f.D.E.e.C.b.A.@.a.B.c.#
-######################.#
-#d.....................#
-########################""".splitlines()) == 86)
-assert(solve("""########################
-#...............b.C.D.f#
-#.######################
-#.....@.a.B.c.d.A.e.F.g#
-########################""".splitlines()) == 132)
-assert(solve("""#################
-#i.G..c...e..H.p#
-########.########
-#j.A..b...f..D.o#
-########@########
-#k.E..a...g..B.n#
-########.########
-#l.F..d...h..C.m#
-#################""".splitlines()) == 136)
-assert(solve("""########################
-#@..............ac.GI.b#
-###d#e#f################
-###A#B#C################
-###g#h#i################
-########################""".splitlines()) == 81)
+def replace_robots(m):
+    x, y = find(m, "@")
+    m[y-1] = m[y-1][:x-1] + "1#2" + m[y-1][x+2:]
+    m[y] = m[y][:x-1] + "###" + m[y][x+2:]
+    m[y+1] = m[y+1][:x-1] + "3#4" + m[y+1][x+2:]
+    return m
+
+
+assert(solve(replace_robots("""#######
+#a.#Cd#
+##...##
+##.@.##
+##...##
+#cB#Ab#
+#######""".splitlines())) == 8)
+assert(solve("""###############
+#d.ABC.#.....a#
+######1#2######
+###############
+######3#4######
+#b.....#.....c#
+###############""".splitlines()) == 24)
+assert(solve("""#############
+#DcBa.#.GhKl#
+#.###1#2#I###
+#e#d#####j#k#
+###C#3#4###J#
+#fEbA.#.FgHi#
+#############""".splitlines()) == 32)
+assert(solve("""#############
+#g#f.D#..h#l#
+#F###e#E###.#
+#dCba1#2BcIJ#
+#############
+#nK.L3#4G...#
+#M###N#H###.#
+#o#m..#i#jk.#
+#############""".splitlines()) == 72)
 
 
 with open('input.txt') as f:
     m = [line.rstrip() for line in f.readlines()]
 
-print(solve(m))
+print(solve(replace_robots(m)))
