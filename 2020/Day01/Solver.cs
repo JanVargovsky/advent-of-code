5d88f55a5fbc7551400b9f4da8eb5b7b25215377
Jan Vargovský <jan.vargovsky@gmail.com>
2020-11-30 19:29:14 +0100

    🏗 Year 2020 Visual Studio C# project and basic structure.

---
+++ 2020/Day01/Solver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode.Year2020.Day01
+{
+    class Solver
+    {
+        public string Solve(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 2020/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using AdventOfCode.Year2020.Day01;
+
+var solver = new Solver();
+
+Debug.Assert(solver.Solve("Foo") == "Bar");
+
+var day = "Day01";
+var path = Path.Combine(day, "input.txt");
+var input = await File.ReadAllTextAsync(path);
+Console.WriteLine(solver.Solve(input));
