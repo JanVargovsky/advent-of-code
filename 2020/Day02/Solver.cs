a46e9011f6b625eba378cf07a439e58e3e9d9a65
Jan Vargovský <jan.vargovsky@gmail.com>
2020-12-02 06:16:16 +0100

    ✨ Add 2020 Day 2

---
+++ 2020/Day02/Solver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Year2020.Day02
+{
+    class Solver
+    {
+        public string Solve(string input)
+        {
+            var passwords = input.Split(Environment.NewLine)
+                .Select(line =>
+                {
+                    var parts = line.Split(new[] { '-', ' ', ':' }, StringSplitOptions.RemoveEmptyEntries);
+                    return new PasswordWithPolicy(
+                        int.Parse(parts[0]),
+                        int.Parse(parts[1]),
+                        parts[2][0],
+                        parts[3]
+                    );
+                }).ToArray();
+
+            return passwords.Count(p =>
+            {
+                var c = p.Password.Count(c => c == p.Character);
+                return c >= p.From && c <= p.To;
+            }).ToString();
+        }
+
+        record PasswordWithPolicy(int From, int To, char Character, string Password);
+    }
+}
--- 2020/Program.cs
+++ 2020/Program.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using AdventOfCode.Year2020.Day01;
+using AdventOfCode.Year2020.Day02;
 
 var solver = new Solver();
 
-//Debug.Assert(solver.Solve(@"1721
-//979
-//366
-//299
-//675
-//1456") == "514579");
+Debug.Assert(solver.Solve(@"1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc") == "2");
 
-var day = "Day01";
+var day = "Day02";
 var path = Path.Combine(day, "input.txt");
 var input = await File.ReadAllTextAsync(path);
 Console.WriteLine(solver.Solve(input));
