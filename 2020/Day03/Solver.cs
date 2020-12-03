e5af53fe5d3a3475d427c7158f3220da1775fac2
Jan Vargovský <jan.vargovsky@gmail.com>
2020-12-03 06:17:51 +0100

    ✨ Add 2020 Day 3

---
+++ 2020/Day03/Solver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Year2020.Day03
+{
+    class Solver
+    {
+        public string Solve(string input)
+        {
+            var array = input.Split(Environment.NewLine)
+                .ToArray();
+
+            var right = 3;
+            var down = 1;
+            var trees = 0;
+
+            while (down < array.Length)
+            {
+                trees += array[down][right] == '#' ? 1 : 0;
+
+                right += 3;
+                right %= array[0].Length;
+                down += 1;
+            }
+
+            return trees.ToString();
+        }
+    }
+}
