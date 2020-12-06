dcb38b0ffbea3536d8ca856e1e95ad76cde57730
Jan Vargovský <jan.vargovsky@gmail.com>
2020-12-06 06:26:40 +0100

    ✨ Add 2020 Day 6

---
+++ 2020/Day06/Solver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AdventOfCode.Year2020.Day06
+{
+    class Solver
+    {
+        public Solver()
+        {
+            Debug.Assert(Solve(@"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b") == "11");
+        }
+
+        public string Solve(string input)
+        {
+            var answers = input.Split(Environment.NewLine + Environment.NewLine)
+                .Select(group => group.Split(Environment.NewLine))
+                .ToArray();
+
+            return answers.Sum(group => group.SelectMany(t => t.ToCharArray()).Distinct().Count())
+                .ToString();
+        }
+    }
+}
