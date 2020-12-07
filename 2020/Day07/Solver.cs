579df76e709ee9adadecccc291dccb23ef8ca2b6
Jan Vargovský <jan.vargovsky@gmail.com>
2020-12-07 07:20:58 +0100

    ✨ Add 2020 Day 7 Part 2

--- 2020/Day07/Solver.cs
+++ 2020/Day07/Solver.cs
@@ -18,7 +18,15 @@ shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
 dark olive bags contain 3 faded blue bags, 4 dotted black bags.
 vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
-dotted black bags contain no other bags.") == "4");
+dotted black bags contain no other bags.") == "32");
+
+            Debug.Assert(Solve(@"shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.") == "126");
         }
 
         public string Solve(string input)
@@ -32,27 +40,19 @@ dotted black bags contain no other bags.") == "4");
                     var nestedBags = rule[(index + separator.Length)..]
                         .Split(", ", StringSplitOptions.RemoveEmptyEntries)
                         .Where(t => !t.StartsWith("no "))
-                        .Select(t => t[2..t.IndexOf(" bag")])
+                        .Select(t => (BagCount: int.Parse(t[..1]), Name: t[2..t.IndexOf(" bag")]))
                         .ToArray();
 
                     return (bag, nestedBags);
                 })
                 .ToDictionary(t => t.bag, t => t.nestedBags);
 
-            var golds = rules.Where(rule => rule.Value.Contains("shiny gold")).Select(t => t.Key).ToHashSet();
+            var result = Solve("shiny gold") - 1;
+            return result.ToString();
 
-            Solve(golds, rules);
-            return golds.Count.ToString();
-
-            void Solve(HashSet<string> bags, Dictionary<string, string[]> rules)
+            int Solve(string name)
             {
-                foreach (var rule in rules)
-                {
-                    if(rule.Value.Any(t => bags.Contains(t)) && bags.Add(rule.Key))
-                    {
-                        Solve(bags, rules);
-                    }
-                }
+                return 1 + rules[name].Sum(t => t.BagCount * Solve(t.Name));
             }
         }
     }
