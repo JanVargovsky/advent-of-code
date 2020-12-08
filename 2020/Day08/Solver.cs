ee5e3328800a7d2e7904d26f1b10fc3bba288712
Jan Vargovský <jan.vargovsky@gmail.com>
2020-12-08 07:08:04 +0100

    ✨ Add 2020 Day 8 Part 2

--- 2020/Day08/Solver.cs
+++ 2020/Day08/Solver.cs
@@ -17,36 +17,52 @@ jmp -3
 acc -99
 acc +1
 jmp -4
-acc +6") == "5");
+acc +6") == "8");
         }
 
         public string Solve(string input)
         {
-            var program = input.Split(Environment.NewLine)
-                .Select(line => (line[..3], int.Parse(line[3..])))
+            var originalProgram = input.Split(Environment.NewLine)
+                .Select(line => (Instruction: line[..3], Argument: int.Parse(line[3..])))
                 .ToArray();
 
-            var acc = 0;
-            var ip = 0;
-            var executed = new HashSet<int>();
-            while (executed.Add(ip))
+            for (int i = 0; i < originalProgram.Length; i++)
             {
-                var (instruction, argument) = program[ip];
-                switch (instruction)
+                foreach (var (from, to) in new[] { ("jmp", "nop"), ("nop", "jmp") })
                 {
-                    case "acc":
-                        acc += argument;
-                        ip++;
-                        break;
-                    case "jmp":
-                        ip += argument;
-                        break;
-                    case "nop":
-                        ip++;
-                        break;
+                    var program = originalProgram.ToArray();
+                    if (program[i].Instruction == from)
+                    {
+                        program[i] = (to, program[i].Argument);
+                    }
+
+                    var acc = 0;
+                    var ip = 0;
+                    var executed = new HashSet<int>();
+                    while (ip < program.Length && executed.Add(ip))
+                    {
+                        var (instruction, argument) = program[ip];
+                        switch (instruction)
+                        {
+                            case "acc":
+                                acc += argument;
+                                ip++;
+                                break;
+                            case "jmp":
+                                ip += argument;
+                                break;
+                            case "nop":
+                                ip++;
+                                break;
+                        }
+                    }
+
+                    if (ip == program.Length)
+                        return acc.ToString();
                 }
             }
-            return acc.ToString();
+
+            throw new Exception();
         }
     }
 }
