2bc6ea9f0886550ab01f3b2e493d9dd8eff53a36
Jan Vargovský <jan.vargovsky@gmail.com>
2020-12-19 10:34:05 +0100

    ✨ Add 2020 Day 19 Part 2

--- 2020/Day19/Solver.cs
+++ 2020/Day19/Solver.cs
@@ -9,21 +9,58 @@ namespace AdventOfCode.Year2020.Day19
     {
         public Solver()
         {
-            Debug.Assert(Solve(@"0: 4 1 5
-1: 2 3 | 3 2
-2: 4 4 | 5 5
-3: 4 5 | 5 4
-4: ""a""
-5: ""b""
+            var input = @"42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: ""a""
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: ""b""
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
 
-ababbb
-bababa
-abbbab
-aaabbb
-aaaabbb") == "2");
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba";
+            Debug.Assert(Solve(input, false) == "3");
+            Debug.Assert(Solve(input) == "12");
         }
 
-        public string Solve(string input)
+        public string Solve(string input, bool updateRules = true)
         {
             var data = input.Split(Environment.NewLine + Environment.NewLine);
 
@@ -47,42 +84,38 @@ aaaabbb") == "2");
                 .ToDictionary(t => t.Id);
             var messages = data[1].Split(Environment.NewLine);
 
-            var result = messages.Count(m => Matches(m, 0, 0) >= m.Length);
-            return result.ToString();
-
-            int Matches(string message, int index, int ruleId)
+            if (updateRules)
             {
-                if (index >= message.Length) return 1;
+                rules[8] = new Rule(8, null, new()
+                {
+                    new() { 42 },
+                    new() { 42, 8 }
+                });
+                rules[11] = new Rule(11, null, new()
+                {
+                    new() { 42, 31 },
+                    new() { 42, 11, 31 }
+                });
+            }
 
-                var rule = rules[ruleId];
+            var matchedMessages = messages
+                .Where(m => Matches(m, 0))
+                .ToList();
+
+            return matchedMessages.Count.ToString();
+
+            bool Matches(string message, params int[] ruleIds)
+            {
+                if (ruleIds.Length == 0) return message.Length == 0;
+                if (message.Length == 0) return false;
+
+                var rule = rules[ruleIds[0]];
                 if (rule.IsTerminal)
                 {
-                    if (rule.Terminal[0] == message[index])
-                        return 1;
-                    else
-                        return -1;
+                    return rule.Terminal[0] == message[0] && Matches(message[1..], ruleIds[1..]);
                 }
 
-                foreach (var subRules in rule.SubRules)
-                {
-                    bool matched = true;
-                    var shift = 0;
-                    for (int i = 0; i < subRules.Count; i++)
-                    {
-                        var r = Matches(message, index + shift, subRules[i]);
-                        if (r == -1)
-                        {
-                            matched = false;
-                            break;
-                        }
-                        shift += r;
-                    }
-
-                    if (matched)
-                        return shift;
-                }
-
-                return -1;
+                return rule.SubRules.Any(subRule => Matches(message, subRule.Concat(ruleIds.Skip(1)).ToArray()));
             }
         }
 
