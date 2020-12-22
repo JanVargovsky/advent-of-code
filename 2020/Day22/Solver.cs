a758eff70e3c4d2a36c9b3acd7677ddac46979ff
Jan Vargovský <jan.vargovsky@gmail.com>
2020-12-22 06:58:02 +0100

    ✨ Add 2020 Day 22 Part 2

--- 2020/Day22/Solver.cs
+++ 2020/Day22/Solver.cs
@@ -21,7 +21,7 @@ Player 2:
 8
 4
 7
-10") == "306");
+10") == "291");
         }
 
         public string Solve(string input)
@@ -31,13 +31,46 @@ Player 2:
                 .ToArray();
             var player1 = decks[0];
             var player2 = decks[1];
+            bool isPlayer1Winner;
+            (player1, player2, isPlayer1Winner) = PlayRecursiveCombat(player1, player2);
+
+            var winner = isPlayer1Winner ? player1 : player2;
+            var result = 0L;
+            while (winner.Count > 0)
+            {
+                result += (winner.Count * winner.Dequeue());
+            }
+            return result.ToString();
+        }
+
+        private static (Queue<int>, Queue<int>, bool) PlayRecursiveCombat(Queue<int> player1, Queue<int> player2)
+        {
+            var prevs = new HashSet<(int, int)>();
 
             while (player1.Count > 0 && player2.Count > 0)
             {
+                if (!prevs.Add((QueueToHash(player1), QueueToHash(player2))))
+                {
+                    return (player1, player2, true);
+                }
+
                 var play1 = player1.Dequeue();
                 var play2 = player2.Dequeue();
 
-                if (play1 > play2)
+                bool isPlayer1Winner;
+                // is subgame needed?
+                if (player1.Count >= play1 && player2.Count >= play2)
+                {
+                    (_, _, isPlayer1Winner) = PlayRecursiveCombat(
+                        new Queue<int>(player1.Take(play1)),
+                        new Queue<int>(player2.Take(play2)));
+                }
+                else
+                {
+                    isPlayer1Winner = play1 > play2;
+                }
+
+                if (isPlayer1Winner)
                 {
                     player1.Enqueue(play1);
                     player1.Enqueue(play2);
@@ -49,13 +82,17 @@ Player 2:
                 }
             }
 
-            var winner = player1.Count > 0 ? player1 : player2;
-            var result = 0L;
-            while (winner.Count > 0)
+            return (player1, player2, player1.Count != 0);
+
+            int QueueToHash(Queue<int> q)
             {
-                result += (winner.Count * winner.Dequeue());
+                int hash = q.Count;
+                foreach (var item in q)
+                {
+                    hash = HashCode.Combine(hash, item);
+                }
+                return hash;
             }
-            return result.ToString();
         }
     }
 }
