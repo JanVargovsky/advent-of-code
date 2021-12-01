af352c2b879c8e9789f5ba6ef674c4c45b5a5e5e
Jan Vargovský <jan.vargovsky@gmail.com>
2021-12-01 06:02:00 +0100

    ✨ Add 2021 Day 1 Part 1

---
+++ 2021/Day01/Solver.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Year2021.Day01;
+
+class Solver
+{
+    public Solver()
+    {
+        Debug.Assert(Solve(@"199
+200
+208
+210
+200
+207
+240
+269
+260
+263") == "7");
+    }
+
+    public string Solve(string input)
+    {
+        var data = input.Split(Environment.NewLine).Select(int.Parse).ToArray();
+        var increased = 0;
+
+        for (int i = 1; i < data.Length; i++)
+        {
+            if (data[i] > data[i - 1])
+                increased++;
+        }
+
+        var result = increased.ToString();
+        return result;
+    }
+}
