3d5885f64b5f58be1daa72ea817f20d6365fb789
Jan Vargovský <jan.vargovsky@gmail.com>
2021-12-06 06:06:12 +0100

    ✨ Add 2021 Day 6 Part 1

---
+++ 2021/Day06/Solver.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventOfCode.Year2021.Day06;
+
+class Solver
+{
+    public Solver()
+    {
+        Debug.Assert(Solve(@"3,4,3,1,2") == "5934");
+    }
+
+    public string Solve(string input)
+    {
+        var lanternfishs = input.Split(',').Select(int.Parse).ToList();
+
+        for (int d = 0; d < 80; d++)
+        {
+            var l = lanternfishs.Count;
+            for (int i = 0; i < l; i++)
+            {
+                if (lanternfishs[i] == 0)
+                {
+                    lanternfishs[i] = 6;
+                    lanternfishs.Add(8);
+                }
+                else
+                {
+                    lanternfishs[i]--;
+                }
+            }
+        }
+
+        var result = lanternfishs.Count;
+        return result.ToString();
+    }
+}
