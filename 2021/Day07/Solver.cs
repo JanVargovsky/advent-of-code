50d32700cc28931f8ee76a0bc1f5a03ae2d0d7b9
Jan Vargovský <jan.vargovsky@gmail.com>
2021-12-07 06:14:54 +0100

    ✨ Add 2021 Day 7 Part 1

---
+++ 2021/Day07/Solver.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode.Year2021.Day07;
+
+class Solver
+{
+    public Solver()
+    {
+        Debug.Assert(Solve(@"16,1,2,0,4,2,7,1,2,14") == "37");
+    }
+
+    public string Solve(string input)
+    {
+        var positions = input.Split(',').Select(long.Parse).ToArray();
+
+        var left = positions.Min();
+        var right = positions.Max();
+        var min = long.MaxValue;
+
+        for (var i = left; i <= right; i++)
+        {
+            min = Math.Min(min, Calculate(i));
+        }
+
+        return min.ToString();
+
+        long Calculate(long align) => positions.Sum(t => Math.Abs(align - t));
+    }
+}
