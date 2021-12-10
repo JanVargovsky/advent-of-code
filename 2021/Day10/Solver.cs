c5b725235edc3ac0fe6a5d6da1a3a827b1081f4c
Jan Vargovský <jan.vargovsky@gmail.com>
2021-12-10 06:10:01 +0100

    ✨ Add 2021 Day 10 Part 1

---
+++ 2021/Day10/Solver.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode.Year2021.Day10;
+
+class Solver
+{
+    public Solver()
+    {
+        Debug.Assert(Solve(@"[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]") == "26397");
+    }
+
+    public string Solve(string input)
+    {
+        var items = input.Split(Environment.NewLine);
+        var mapping = new Dictionary<char, char>()
+        {
+            [')'] = '(',
+            [']'] = '[',
+            ['}'] = '{',
+            ['>'] = '<',
+        };
+
+        var result = items.Sum(Validate);
+
+        return result.ToString();
+
+        int Validate(string line)
+        {
+            var s = new Stack<char>();
+
+            foreach (var c in line)
+            {
+                if (mapping.TryGetValue(c, out var start))
+                {
+                    var lastStart = s.Pop();
+                    if (start != lastStart)
+                    {
+                        return start switch
+                        {
+                            '(' => 3,
+                            '[' => 57,
+                            '{' => 1197,
+                            '<' => 25137,
+                        };
+                    }
+                }
+                else
+                {
+                    s.Push(c);
+                }
+            }
+
+            return 0;
+        }
+    }
+}
