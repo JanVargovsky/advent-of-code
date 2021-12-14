c5d7be8b6a06c94d4edff7553e0d75f9ddcd0922
Jan Vargovský <jan.vargovsky@gmail.com>
2021-12-14 07:41:06 +0100

    ✨ Add 2021 Day 14 Part 2

--- 2021/Day14/Solver.cs
+++ 2021/Day14/Solver.cs
@@ -21,42 +21,71 @@ BN -> B
 BB -> N
 BC -> B
 CC -> N
-CN -> C") == "1588");
+CN -> C", 10) == "1588");
+        Debug.Assert(Solve(@"NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C") == "2188189693529");
     }
 
-    public string Solve(string input)
+    public string Solve(string input, int steps = 40)
     {
         var items = input.Split(Environment.NewLine);
-        var template = new LinkedList<char>(items[0]);
+        var template = items[0];
         var rules = new Dictionary<string, char>(items[2..].Select(rule =>
         {
             var index = rule.IndexOf(" -> ");
             return new KeyValuePair<string, char>(rule[..index], rule[^1]);
         }));
 
-        for (int i = 0; i < 10; i++)
+        var counts = new Dictionary<string, ulong>();
+        foreach (var (a, b) in template.Zip(template.Skip(1)))
         {
-            ApplyStep();
+            var key = $"{a}{b}";
+            counts[key] = counts.GetValueOrDefault(key) + 1;
         }
 
-        var counts = template.GroupBy(t => t).ToDictionary(t => t.Key, t => t.Count());
-        var mostCommon = counts.MaxBy(t => t.Value);
-        var leastCommon = counts.MinBy(t => t.Value);
+        for (int i = 0; i < steps; i++)
+        {
+            var newCounts = new Dictionary<string, ulong>();
+            foreach (var item in counts)
+            {
+                var value = rules[item.Key];
+                var count = item.Value;
+                var a = $"{item.Key[0]}{value}";
+                var b = $"{value}{item.Key[1]}";
+                newCounts[a] = newCounts.GetValueOrDefault(a) + count;
+                newCounts[b] = newCounts.GetValueOrDefault(b) + count;
+            }
+            counts = newCounts;
+        }
+
+        var resultCounts = new Dictionary<char, ulong>();
+        foreach (var item in counts)
+        {
+            var c = item.Key[0];
+            if (!resultCounts.TryAdd(c, item.Value))
+                resultCounts[c] += item.Value;
+        }
+        resultCounts[template[^1]] += 1;
+        var mostCommon = resultCounts.MaxBy(t => t.Value);
+        var leastCommon = resultCounts.MinBy(t => t.Value);
 
         var result = mostCommon.Value - leastCommon.Value;
         return result.ToString();
-
-        void ApplyStep()
-        {
-            var current = template.First;
-            while (current != null && current.Next != null)
-            {
-                var value = $"{current.Value}{current.Next.Value}";
-                var next = current.Next;
-                var rule = rules[value];
-                template.AddAfter(current, rule);
-                current = next;
-            }
-        }
     }
 }
