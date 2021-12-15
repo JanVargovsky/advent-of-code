1dce52f177b0b987260eee8a241488786dab0166
Jan Vargovský <jan.vargovsky@gmail.com>
2021-12-15 07:13:13 +0100

    ✨ Add 2021 Day 15 Part 2

--- 2021/Day15/Solver.cs
+++ 2021/Day15/Solver.cs
@@ -13,13 +13,28 @@ class Solver
 1359912421
 3125421639
 1293138521
-2311944581") == "40");
+2311944581") == "315");
     }
 
     public string Solve(string input)
     {
-        var map = input.Split(Environment.NewLine);
-        var length = map.Length;
+        var tile = input.Split(Environment.NewLine);
+        var length = tile.Length * 5;
+        var map = new int[length, length];
+        for (int y = 0; y < length; y++)
+        {
+            for (int x = 0; x < length; x++)
+            {
+                var (scaleX, xi) = Math.DivRem(x, tile.Length);
+                var (scaleY, yi) = Math.DivRem(y, tile.Length);
+                var value = tile[yi][xi] - '0';
+                value += scaleX + scaleY;
+                if (value > 9)
+                    value %= 9;
+                map[x, y] = value;
+            }
+        }
+        //PrintMap();
         var distances = Calculate();
         var result = distances[length - 1, length - 1] - distances[0, 0];
         return result.ToString();
@@ -37,20 +52,34 @@ class Solver
             while (q.Count > 0)
             {
                 var (x, y, pathCost) = q.Dequeue();
-                if (x >= length || y >= length)
+                if (x < 0 || y < 0 || x >= length || y >= length)
                     continue;
 
-                var cost = map[y][x] - '0';
+                var cost = map[x, y];
                 pathCost += cost;
                 if (distances[x, y] > pathCost)
                 {
                     distances[x, y] = pathCost;
                     q.Enqueue((x + 1, y, pathCost));
+                    q.Enqueue((x - 1, y, pathCost));
                     q.Enqueue((x, y + 1, pathCost));
+                    q.Enqueue((x, y - 1, pathCost));
                 }
             }
 
             return distances;
+        }
+
+        void PrintMap()
+        {
+            for (int y = 0; y < length; y++)
+            {
+                for (int x = 0; x < length; x++)
+                {
+                    Console.Write(map[x, y]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
