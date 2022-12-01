d817da70cdce1352544e2998e1ff321ffe46c4d0
Jan Vargovský <jan.vargovsky@gmail.com>
2022-12-01 06:06:37 +0100

    ✨ Add 2022 Day 1 Part 1

---
+++ 2022/Day01/Solver.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Year2022.Day01;
+
+internal class Solver
+{
+    public Solver()
+    {
+        Debug.Assert(Solve("""
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+""") == "24000");
+    }
+
+    public string Solve(string input)
+    {
+        var data = input.Split(Environment.NewLine + Environment.NewLine);
+        var sums = data.Select(t => t.Split(Environment.NewLine).Select(long.Parse).Sum());
+
+        var result = sums.Max();
+        return result.ToString();
+    }
+}
