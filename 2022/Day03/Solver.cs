59a5840bc1a45a82aef8812a138ec3025d905fdb
Jan Vargovský <jan.vargovsky@gmail.com>
2022-12-03 06:13:01 +0100

    ✨ Add 2022 Day 3 Part 1

---
+++ 2022/Day03/Solver.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode.Year2022.Day03;
+
+internal class Solver
+{
+    public Solver()
+    {
+        Debug.Assert(Solve("""
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+""") == "157");
+    }
+
+    public string Solve(string input)
+    {
+        var rucksacks = input.Split(Environment.NewLine);
+
+        var result = 0;
+
+        foreach (var item in rucksacks)
+        {
+            var mid = item.Length / 2;
+
+            var first = item[..mid];
+            var second = item[mid..];
+
+            var shared = first.Intersect(second).First();
+
+            var score = 0;
+            if (shared is >= 'a' and <= 'z')
+                score = shared - 'a' + 1;
+            else
+                score = shared - 'A' + 27;
+
+            result += score;
+        }
+
+        return result.ToString();
+    }
+}
