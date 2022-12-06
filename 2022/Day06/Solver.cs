a59b849efca2ba93b207afc7f7fff1a93f8944a9
Jan Vargovský <jan.vargovsky@gmail.com>
2022-12-06 06:22:17 +0100

    ✨ Add 2022 Day 6 Part 1

---
+++ 2022/Day06/Solver.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode.Year2022.Day06;
+
+internal class Solver
+{
+    public Solver()
+    {
+        Debug.Assert(Solve("""
+mjqjpqmgbljsphdztnvjfqwrcgsmlb
+""") == 7);
+        Debug.Assert(Solve("""
+bvwbjplbgvbhsrlpgdmjqwftvncz
+""") == 5);
+        Debug.Assert(Solve("""
+nppdvjthqldpwncqszvftbrmjlhg
+""") == 6);
+        Debug.Assert(Solve("""
+nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
+""") == 10);
+        Debug.Assert(Solve("""
+zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
+""") == 11);
+    }
+
+    public int Solve(string input)
+    {
+        const int markerLength = 4;
+        var i = markerLength;
+        while (i < input.Length && input[(i - markerLength)..i].ToHashSet().Count != markerLength)
+        {
+            i++;
+        }
+
+        return i;
+    }
+}
