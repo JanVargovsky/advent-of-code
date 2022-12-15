7792915533f00e3f7c828a1a600b3496769e7d94
Jan Vargovský <jan.vargovsky@gmail.com>
2022-12-15 12:25:04 +0100

    ✨ Add 2022 Day 15 Part 2

--- 2022/Day15/Solver.cs
+++ 2022/Day15/Solver.cs
@@ -6,9 +6,17 @@ internal class Solver
 {
     public Solver()
     {
-        Debug.Assert(Solve("""
-Sensor at x=8, y=7: closest beacon is at x=2, y=10
-""", 10) == 12);
+        Debug.Assert(Merge(new Interval[]
+        {
+            new(0, 10),
+            new(5,15),
+            new(2,5),
+            new(20,30)
+        }).SequenceEqual(new Interval[]
+        {
+            new(0, 15),
+            new(20,30)
+        }));
 
         Debug.Assert(Solve("""
 Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -25,38 +33,67 @@ Sensor at x=17, y=20: closest beacon is at x=21, y=22
 Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
 Sensor at x=20, y=1: closest beacon is at x=15, y=3
-""", 10) == 26);
+""", 20) == 56000011);
     }
 
-    public int Solve(string input, int targetY = 2000000)
+    public long Solve(string input, int limit = 4000000)
     {
         var rows = input.Split(Environment.NewLine);
-        var sensors = new HashSet<Point>();
-        var beacons = new HashSet<Point>();
-        var targetRow = new HashSet<int>(); // X
-        foreach (var row in rows)
+        var items = rows.Select(row =>
         {
             var tokens = row.Split('=', ',', ':');
             var sensor = new Point(int.Parse(tokens[1]), int.Parse(tokens[3]));
             var beacon = new Point(int.Parse(tokens[5]), int.Parse(tokens[7]));
-            sensors.Add(sensor);
-            beacons.Add(beacon);
+            return (sensor, beacon);
+        });
 
-            var distance = ManhattanDistance(sensor, beacon);
-            var distanceToTarget = ManhattanDistance(sensor, new Point(sensor.X, targetY));
-            if (distanceToTarget < distance)
+        for (int y = 0; y <= limit; y++)
+        {
+            var row = new List<Interval>();
+            foreach (var (sensor, beacon) in items)
             {
-                var diff = distance - distanceToTarget;
-                var numbers = Enumerable.Range(sensor.X - diff, diff * 2 + 1);
-                foreach (var item in numbers)
+                var distance = ManhattanDistance(sensor, beacon);
+                var distanceToTarget = ManhattanDistance(sensor, new Point(sensor.X, y));
+                if (distanceToTarget < distance)
                 {
-                    targetRow.Add(item);
+                    var diff = distance - distanceToTarget;
+                    row.Add(new(sensor.X - diff, sensor.X + diff));
                 }
+            }
+
+            var sortedRow = row.OrderBy(t => t.From).ThenBy(t => t.To).ToArray();
+            var merged = Merge(sortedRow);
+
+            if (merged.Count == 2)
+            {
+                var x = merged[0].To + 1;
+                return x * 4000000L + y;
             }
         }
 
-        var beaconsOnTarget = beacons.Where(t => t.Y == targetY).Select(t => t.X);
-        var result = targetRow.Except(beaconsOnTarget).Count();
+        throw new Exception();
+    }
+
+    private List<Interval> Merge(IList<Interval> items)
+    {
+        var result = new List<Interval>();
+        var bounds = items[0];
+
+        for (int i = 1; i < items.Count; i++)
+        {
+            var interval = items[i];
+            if (interval.From <= bounds.To)
+            {
+                bounds.To = Math.Max(bounds.To, interval.To);
+            }
+            else
+            {
+                result.Add(bounds);
+                bounds = interval;
+            }
+        }
+        result.Add(bounds);
+
         return result;
     }
 
@@ -73,3 +110,5 @@ internal record struct Point(int X, int Y) : IAdditionOperators<Point, Point, Po
         return new(left.X + right.X, left.Y + right.Y);
     }
 }
+
+internal record struct Interval(int From, int To);
