395c2af5b3e2d649a25fcaaad8c5b6a377b43966
Jan Vargovský <jan.vargovsky@gmail.com>
2022-12-18 11:02:05 +0100

    ✨ Add 2022 Day 18 Part 2

--- 2022/Day18/Solver.cs
+++ 2022/Day18/Solver.cs
@@ -7,6 +7,9 @@ internal class Solver
     public Solver()
     {
         Debug.Assert(Solve("""
+0,0,0
+""") == 6);
+        Debug.Assert(Solve("""
 1,1,1
 2,1,1
 """) == 10);
@@ -24,7 +27,7 @@ internal class Solver
 3,2,5
 2,1,5
 2,3,5
-""") == 64);
+""") == 58);
     }
 
     public int Solve(string input)
@@ -35,16 +38,63 @@ internal class Solver
             return new Point(int.Parse(tokens[0]), int.Parse(tokens[1]), int.Parse(tokens[2]));
         }).ToHashSet();
 
-        var result = 0;
-        foreach (var cube in cubes)
+        var queue = new Queue<Point>();
+        var visited = new HashSet<Point>();
+        var min = new Point(cubes.Min(t => t.X) - 1, cubes.Min(t => t.Y) - 1, cubes.Min(t => t.Z) - 1);
+        var max = new Point(cubes.Max(t => t.X) + 1, cubes.Max(t => t.Y) + 1, cubes.Max(t => t.Z) + 1);
+        var start = min;
+        Debug.Assert(!cubes.Contains(start));
+        queue.Enqueue(start);
+
+        while (queue.TryDequeue(out var cube))
         {
+            if (cube.X < min.X || cube.X > max.X ||
+                    cube.Y < min.Y || cube.Y > max.Y ||
+                    cube.Z < min.Z || cube.Z > max.Z)
+                continue;
+
+            if (cubes.Contains(cube))
+                continue;
+
+            if (!visited.Add(cube))
+                continue;
+
             foreach (var neighbor in GetNeighbors(cube))
             {
-                if (!cubes.Contains(neighbor))
-                    result++;
+                queue.Enqueue(neighbor);
             }
         }
+        var outside = visited;
+        var inside = new HashSet<Point>();
+        for (int x = min.X; x <= max.X; x++)
+        {
+            for (int y = min.Y; y <= max.Y; y++)
+            {
+                for (int z = min.Z; z <= max.Z; z++)
+                {
+                    var p = new Point(x, y, z);
+                    if (!outside.Contains(p))
+                        inside.Add(p);
+                }
+            }
+        }
+
+        var result = CalculateSurface(inside);
         return result;
+
+        int CalculateSurface(HashSet<Point> cubes)
+        {
+            int result = 0;
+            foreach (var cube in cubes)
+            {
+                foreach (var neighbor in GetNeighbors(cube))
+                {
+                    if (!cubes.Contains(neighbor))
+                        result++;
+                }
+            }
+            return result;
+        }
 
         IEnumerable<Point> GetNeighbors(Point a)
         {
