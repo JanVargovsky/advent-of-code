3c8e5a4969bb3a30bfce39561071c5e438bd394f
Jan Vargovský <jan.vargovsky@gmail.com>
2023-12-01 08:05:33 +0100

    ✨ Add 2022 Day 1 Part 2

--- 2023/Day01/Solver.cs
+++ 2023/Day01/Solver.cs
@@ -5,24 +5,70 @@ internal class Solver
     public Solver()
     {
         Debug.Assert(Solve("""
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
-""") == 142);
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+""") == 281);
     }
 
     public int Solve(string input)
     {
         var rows = input.Split(Environment.NewLine);
         var result = 0;
+        var numbers = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
         foreach (var row in rows)
         {
-            var first = row.First(char.IsDigit);
-            var last = row.Last(char.IsDigit);
+            var first = Find(row);
+            var last = FindLast(row);
+
             var number = Convert.ToInt32($"{first}{last}");
             result += number;
         }
         return result;
+
+        int FindLast(string row)
+        {
+            for (int i = row.Length - 1; i >= 0; i--)
+            {
+                if (char.IsDigit(row[i]))
+                {
+                    return row[i] - '0';
+                }
+                else
+                {
+                    for (int j = 0; j < numbers.Length; j++)
+                    {
+                        if (row[i..].StartsWith(numbers[j]))
+                            return j + 1;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        int Find(string row)
+        {
+            for (int i = 0; i < row.Length; i++)
+            {
+                if (char.IsDigit(row[i]))
+                {
+                    return row[i] - '0';
+                }
+                else
+                {
+                    for (int j = 0; j < numbers.Length; j++)
+                    {
+                        if (row[i..].StartsWith(numbers[j]))
+                            return j + 1;
+                    }
+                }
+            }
+            return -1;
+        }
     }
 }
