f84a0dde894d4aa0aa07e3351991a9fddec4a65c
Jan Vargovský <jan.vargovsky@gmail.com>
2023-12-04 09:30:40 +0100

    ✨ Add 2022 Day 4 Part 2

--- 2023/Day04/Solver.cs
+++ 2023/Day04/Solver.cs
@@ -13,25 +13,34 @@ Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-""") == 13);
+""") == 30);
     }
 
     public int Solve(string input)
     {
         var rows = input.Split(Environment.NewLine);
-        var scratchcards = rows.Select(Parse).ToArray();
+        var scratchcards = rows.Select(Parse).ToList();
         List<int> points = [1, 1];
         while (points.Count <= scratchcards[0].Winning.Count)
             points.Add(points[^1] * 2);
+        scratchcards.ForEach(card => (card.WinningCards, card.Points) = CalculatePoints(card));
 
-        var result = scratchcards.Sum(CalculatePoints);
+        for (int i = 0; i < scratchcards.Count; i++)
+        {
+            for (int j = 1; j <= scratchcards[i].WinningCards; j++)
+            {
+                scratchcards[i + j].Count += (scratchcards[i].Count);
+            }
+        }
+
+        var result = scratchcards.Sum(t => t.Count);
         return result;
 
-        int CalculatePoints(Scratchcard card)
+        (int Cards, int Points) CalculatePoints(Scratchcard card)
         {
             var winning = card.Winning.Intersect(card.Owned);
-            var result = winning.Select((_, index) => points[index]).Sum();
-            return result;
+            var winningPoints = winning.Select((_, index) => points[index]).Sum();
+            return (winning.Count, winningPoints);
         }
 
         Scratchcard Parse(string row)
@@ -45,5 +54,10 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
         }
     }
 
-    private record Scratchcard(int Id, ImmutableHashSet<int> Winning, ImmutableHashSet<int> Owned);
+    private record Scratchcard(int Id, ImmutableHashSet<int> Winning, ImmutableHashSet<int> Owned)
+    {
+        public int WinningCards { get; set; }
+        public int Points { get; set; }
+        public int Count { get; set; } = 1;
+    }
 }
