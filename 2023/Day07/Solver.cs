6d06446a8e8150294866fbbb8470189d56de92e7
Jan Vargovský <jan.vargovsky@gmail.com>
2023-12-07 10:34:51 +0100

    ✨ Add 2022 Day 7 Part 2
    Check your constants next time, you will save time = don't add duplicate J to the sorted cards.

--- 2023/Day07/Solver.cs
+++ 2023/Day07/Solver.cs
@@ -19,6 +19,32 @@ internal class Solver
             Debug.Assert(hand2.HandResult is HandResult.FullHouse);
             Debug.Assert(comparer.Compare(hand1, hand2) > 0);
         }
+        {
+            var hand = new Hand("QJJQ2", 0);
+            Debug.Assert(hand.HandResult is HandResult.FourOfKind);
+        }
+        {
+            var hand1 = new Hand("JKKK2", 0);
+            Debug.Assert(hand1.HandResult is HandResult.FourOfKind);
+            var hand2 = new Hand("QQQQ2", 0);
+            Debug.Assert(hand2.HandResult is HandResult.FourOfKind);
+            Debug.Assert(comparer.Compare(hand1, hand2) < 0);
+        }
+        {
+            var hand = new Hand("JJJ22", 0);
+            Debug.Assert(hand.HandResult is HandResult.FiveOfKind);
+        }
+        {
+            var hand = new Hand("JJ22X", 0);
+            Debug.Assert(hand.HandResult is HandResult.FourOfKind);
+        }
+        {
+            var hand1 = new Hand("2JJJJ", 0);
+            Debug.Assert(hand1.HandResult is HandResult.FiveOfKind);
+            var hand2 = new Hand("JJJJ2", 0);
+            Debug.Assert(hand2.HandResult is HandResult.FiveOfKind);
+            Debug.Assert(comparer.Compare(hand1, hand2) > 0);
+        }
 
         Debug.Assert(Solve("""
 32T3K 765
@@ -26,7 +52,25 @@ T55J5 684
 KK677 28
 KTJJT 220
 QQQJA 483
-""") == 6440);
+""") == 5905);
+
+        Debug.Assert(Solve("""
+2345A 2
+2345J 5
+J345A 3
+32T3K 7
+T55J5 17
+KK677 11
+KTJJT 23
+QQQJA 19
+JJJJJ 29
+JAAAA 37
+AAAAJ 43
+AAAAA 53
+2AAAA 13
+2JJJJ 41
+JJJJ2 31
+""") == 3667);
     }
 
     public long Solve(string input)
@@ -45,7 +89,7 @@ QQQJA 483
 
     private class HandComparer : IComparer<Hand>
     {
-        private const string Cards = "AKQJT98765432";
+        private const string Cards = "AKQT98765432J";
 
         public int Compare(Hand? x, Hand? y)
         {
@@ -58,7 +102,9 @@ QQQJA 483
             for (int i = 0; i < x.Cards.Length; i++)
             {
                 var indexX = Cards.IndexOf(x.Cards[i]);
+                Debug.Assert(indexX >= 0);
                 var indexY = Cards.IndexOf(y.Cards[i]);
+                Debug.Assert(indexX >= 0);
                 result = indexY.CompareTo(indexX); // inverse
                 if (result is not 0)
                     return result;
@@ -75,10 +121,13 @@ QQQJA 483
         private static HandResult GetResult(string cards)
         {
             var groups = cards.GroupBy(t => t, t => t).ToDictionary(t => t.Key, t => t.Count());
+            var hasJokers = groups.Remove('J', out var jokersCount);
+            if (groups.Count == 0)
+                return HandResult.FiveOfKind;
 
             var highestPair = groups.MaxBy(t => t.Value);
             var secondHighestCount = groups.Count >= 2 ? groups.Where(t => t.Key != highestPair.Key).Max(t => t.Value) : 0;
-            var result = highestPair.Value switch
+            var result = (highestPair.Value + jokersCount) switch
             {
                 5 => HandResult.FiveOfKind,
                 4 => HandResult.FourOfKind,
