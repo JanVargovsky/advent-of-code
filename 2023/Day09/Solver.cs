5a814ddeaa389c738a0e328711d77890a65365e2
Jan Vargovský <jan.vargovsky@gmail.com>
2023-12-09 12:20:19 +0100

    ✨ Add 2022 Day 9 Part 2

--- 2023/Day09/Solver.cs
+++ 2023/Day09/Solver.cs
@@ -4,21 +4,27 @@ internal class Solver
 {
     public Solver()
     {
+        Debug.Assert(PredictPreceding([10, 13, 16, 21, 30, 45]) == 5);
         Debug.Assert(Solve("""
 0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45
-""") == 114);
+""") == 2);
     }
 
     public int Solve(string input)
     {
         var rows = input.Split(Environment.NewLine).Select(Parse).ToArray();
-        var predictions = rows.Select(PredictNext);
+        var predictions = rows.Select(PredictPreceding);
         var result = predictions.Sum();
         return result;
 
         int[] Parse(string row) => row.Split().Select(int.Parse).ToArray();
+    }
+
+    private int PredictPreceding(int[] values)
+    {
+        return PredictNext(values.Reverse().ToArray());
     }
 
     private int PredictNext(int[] values)
@@ -35,6 +41,8 @@ internal class Solver
         for (int i = sequences.Count - 1 - 1; i >= 0; i--)
         {
             predicts[i] = sequences[i][^1] + predicts[i + 1];
+            // predicts[i] = sequences[i][0] - predicts[i + 1]; // if you want PredictPreceding without Reverse
+
         }
 
         return predicts[0];
