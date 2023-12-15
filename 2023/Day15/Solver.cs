2506bae5add2f1a80e46da6dec30c32114f9d9b1
Jan Vargovský <jan.vargovsky@gmail.com>
2023-12-15 08:28:07 +0100

    ✨ Add 2022 Day 15 Part 2

--- 2023/Day15/Solver.cs
+++ 2023/Day15/Solver.cs
@@ -6,15 +6,51 @@ internal class Solver
     {
         Debug.Assert(Solve("""
 rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
-""") == 1320);
+""") == 145);
     }
 
     public int Solve(string input)
     {
         var values = input.Split(',');
-        var results = values.Select(Hash);
-        var result = results.Sum();
+        var boxes = new List<Lens>[256];
+        foreach (var str in values)
+        {
+            if (str[^1] == '-')
+            {
+                var name = str[..^1];
+                var hash = Hash(name);
+                var box = boxes[hash];
+                if (box is null)
+                    continue;
+                var removed = box.RemoveAll(t => t.Name == name);
+            }
+            else
+            {
+                var tokens = str.Split('=');
+                var name = tokens[0];
+                var value = int.Parse(tokens[1]);
+                var hash = Hash(name);
+                var lens = new Lens(name, value);
+                var box = boxes[hash] ??= [];
+                var indexOfSame = box.FindIndex(t => t.Name == lens.Name);
+                if (indexOfSame == -1)
+                    box.Add(lens);
+                else
+                    box[indexOfSame] = lens;
+            }
+        }
+        var result = boxes.Select((boxes, index) => FocusingPower(index + 1, boxes)).Sum();
         return result;
+
+        int FocusingPower(int box, List<Lens> lenses)
+        {
+            if (lenses is null or { Count: 0 })
+                return 0;
+
+            var results = lenses.Select((lens, index) => box * (index + 1) * lens.Value);
+            var result = results.Sum();
+            return result;
+        }
 
         int Hash(string str)
         {
@@ -28,4 +64,6 @@ rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
             return value;
         }
     }
+
+    private record Lens(string Name, int Value);
 }
