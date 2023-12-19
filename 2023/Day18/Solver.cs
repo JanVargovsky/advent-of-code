6da600bdee9f767578a2f33fa53fb8a5025a6d68
Jan Vargovský <jan.vargovsky@gmail.com>
2023-12-19 09:38:30 +0100

    ✨ Add 2022 Day 18 Part 2

--- 2023/Day18/Solver.cs
+++ 2023/Day18/Solver.cs
@@ -7,12 +7,6 @@ internal class Solver
     public Solver()
     {
         Debug.Assert(Solve("""
-R 4 (#70c710)
-D 3 (#0dc571)
-L 4 (#5713f0)
-U 3 (#d2c081)
-""") == 20);
-        Debug.Assert(Solve("""
 R 6 (#70c710)
 D 5 (#0dc571)
 L 2 (#5713f0)
@@ -27,10 +21,10 @@ R 2 (#7807d2)
 U 3 (#a77fa3)
 L 2 (#015232)
 U 2 (#7a21e3)
-""") == 62);
+""") == 952408144115);
     }
 
-    public int Solve(string input)
+    public long Solve(string input)
     {
         var rows = input.Split(Environment.NewLine);
         var directions = new Dictionary<char, Point2d>
@@ -40,45 +34,43 @@ U 2 (#7a21e3)
             ['U'] = new(-1, 0),
             ['D'] = new(1, 0),
         };
+        directions['0'] = directions['R'];
+        directions['1'] = directions['D'];
+        directions['2'] = directions['L'];
+        directions['3'] = directions['U'];
+
         var current = new Point2d(0, 0);
+        var length = 0;
         var points = new HashSet<Point2d>();
         points.Add(current);
-        var length = 0;
-        var edges = new HashSet<Point2d>();
-        edges.Add(current);
 
         foreach (var row in rows)
         {
             var tokens = row.Split(' ');
-            var direction = directions[tokens[0][0]];
-            var distance = int.Parse(tokens[1]);
-            for (int i = 0; i < distance; i++)
-            {
-                current += direction;
-                points.Add(current);
-            }
+            var hex = tokens[2][2..^1];
+            var distance = Convert.ToInt32(hex[..^1], 16);
+            var direction = directions[hex[^1]];
+            current += direction * distance;
             length += distance;
-            edges.Add(current);
+            points.Add(current);
         }
 
-        //Print(edges);
         //Print(points);
 
         var result = CalculateCount();
         return result;
 
-        int CalculateCount()
+        long CalculateCount()
         {
             // https://en.wikipedia.org/wiki/Polygon#Simple_polygons => https://en.m.wikipedia.org/wiki/Shoelace_formula#Triangle_formula
-            var area = Math.Abs(edges.Zip(edges.Skip(1)).Select(t => ShoelaceFormula(t.First, t.Second)).Sum()) / 2;
+            var area = Math.Abs(points.Zip(points.Skip(1)).Select(t => ShoelaceFormula(t.First, t.Second)).Sum()) / 2;
 
-            var boundaryPoints = edges.Count;
             var interiorArea = PicksTheorem(area, length);
-            var result = interiorArea + points.Count;
+            var result = interiorArea + length;
             return result;
 
-            static int ShoelaceFormula(Point2d a, Point2d b) => a.Row * b.Column - b.Row * a.Column;
-            static int PicksTheorem(int area, int boundaryPoints) => area - boundaryPoints / 2 + 1;
+            static long ShoelaceFormula(Point2d a, Point2d b) => (long)a.Row * b.Column - (long)b.Row * a.Column;
+            static long PicksTheorem(long area, long boundaryPoints) => area - boundaryPoints / 2 + 1;
         }
 
         void Print(HashSet<Point2d> points)
@@ -103,5 +95,7 @@ U 2 (#7a21e3)
     private record Point2d(int Row, int Column) : IAdditionOperators<Point2d, Point2d, Point2d>
     {
         public static Point2d operator +(Point2d left, Point2d right) => new(left.Row + right.Row, left.Column + right.Column);
+        public static Point2d operator *(Point2d left, int value) => new(left.Row * value, left.Column * value);
+
     }
 }
