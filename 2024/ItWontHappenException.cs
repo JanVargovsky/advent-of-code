06c4ec145ee0536cbfcd8b27804f439dbe3326c8
Jan Vargovský <Jan.Vargovsky@rws.com>
2024-12-07 11:33:02 +0100

    🏗 2024 project

---
+++ 2024/ItWontHappenException.cs
@@ -0,0 +1,7 @@
+﻿namespace AdventOfCode.Year2024;
+
+public class ItWontHappenException : Exception
+{
+    public ItWontHappenException() { }
+    public ItWontHappenException(string message) : base(message) { }
+}
