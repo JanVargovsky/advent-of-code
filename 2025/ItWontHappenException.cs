d54e7e897568e1596a09154fe597690342e9013b
Jan Vargovský <Jan.Vargovsky@rws.com>
2025-12-01 10:34:04 +0100

    🎉 2025 solution

---
+++ 2025/ItWontHappenException.cs
@@ -0,0 +1,7 @@
+﻿namespace AdventOfCode.Year2025;
+
+public class ItWontHappenException : Exception
+{
+    public ItWontHappenException() { }
+    public ItWontHappenException(string message) : base(message) { }
+}
