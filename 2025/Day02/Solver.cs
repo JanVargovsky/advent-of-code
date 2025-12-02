1d84db5d8d5f664711ffd72e216988362d4e12c0
Jan Vargovský <Jan.Vargovsky@rws.com>
2025-12-02 11:11:40 +0100

    ✨ Add 2025 Day 2 Part 2

--- 2025/Day02/Solver.cs
+++ 2025/Day02/Solver.cs
@@ -4,11 +4,16 @@ internal class Solver
 {
     public Solver()
     {
+        Debug.Assert(IsInvalid("11"));
+        Debug.Assert(!IsInvalid("12"));
+        Debug.Assert(IsInvalid("22"));
+        Debug.Assert(IsInvalid("999"));
+        Debug.Assert(IsInvalid("1010"));
         Debug.Assert(Solve("""
 11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
 1698522-1698528,446443-446449,38593856-38593862,565653-565659,
 824824821-824824827,2121212118-2121212124
-""") == 1227775554);
+""") == 4174379265);
     }
 
     public long Solve(string input)
@@ -31,12 +36,28 @@ internal class Solver
 
     private bool IsInvalid(ReadOnlySpan<char> input)
     {
-        var (n, rem) = Math.DivRem(input.Length, 2);
-        if (rem != 0)
-            return false;
+        for (int i = 1; i <= input.Length / 2; i++)
+        {
+            var (n, rem) = Math.DivRem(input.Length, i);
+            if (rem != 0)
+                continue;
 
-        var left = input[..n];
-        var right = input[n..];
-        return left.SequenceEqual(right);
+            var left = input[..i];
+            var invalid = false;
+            for (var j = 1; j < n; j++)
+            {
+                var right = input[(i * j)..(i * (j + 1))];
+                if (!left.SequenceEqual(right))
+                {
+                    invalid = true;
+                    break;
+                }
+            }
+
+            if (!invalid)
+                return true;
+        }
+
+        return false;
     }
 }
