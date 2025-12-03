405ca6f9781b4567b3e420ab2748a43972bb349e
Jan Vargovský <Jan.Vargovsky@rws.com>
2025-12-03 13:23:59 +0100

    ✨ Add 2025 Day 3 Part 2

--- 2025/Day03/Solver.cs
+++ 2025/Day03/Solver.cs
@@ -1,17 +1,20 @@
-using MoreLinq;
-
 namespace AdventOfCode.Year2025.Day03;
 
 internal class Solver
 {
     public Solver()
     {
+        Debug.Assert(MaximumJoltage("987654321111111") == 987654321111);
+        Debug.Assert(MaximumJoltage("811111111111119") == 811111111119);
+        Debug.Assert(MaximumJoltage("234234234234278") == 434234234278);
+        Debug.Assert(MaximumJoltage("818181911112111") == 888911112111);
+
         Debug.Assert(Solve("""
 987654321111111
 811111111111119
 234234234234278
 818181911112111
-""") == 357);
+""") == 3121910778619);
     }
 
     public long Solve(string input)
@@ -29,9 +32,22 @@ internal class Solver
 
     private long MaximumJoltage(string input)
     {
+        const int Length = 12;
         const char CharOffset = '0';
-        return input.Subsets(2)
-            .Select(items => items.Aggregate(0L, (a, c) => a * 10 + c - CharOffset))
-            .Max();
+        var result = 0L;
+        var last = -1;
+
+        for (var i = 0; i < Length; i++)
+        {
+            var tmp = input[(last + 1)..(input.Length - Length + i + 1)]
+                .Select((c, i) => (Digit: c - CharOffset, Index: i + last + 1))
+                .MaxBy(t => t.Digit);
+
+            result *= 10;
+            result += tmp.Digit;
+            last = tmp.Index;
+        }
+
+        return result;
     }
 }
