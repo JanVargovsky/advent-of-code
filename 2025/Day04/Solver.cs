3aced052604f6f2dcac7c8ce574abf4270b9af66
Jan Vargovský <Jan.Vargovsky@rws.com>
2025-12-04 11:13:16 +0100

    ✨ Add 2025 Day 4 Part 2

--- 2025/Day04/Solver.cs
+++ 2025/Day04/Solver.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace AdventOfCode.Year2025.Day04;
 
 internal class Solver
@@ -15,26 +17,47 @@ internal class Solver
 @.@@@.@@@@
 .@@@@@@@@.
 @.@.@@@.@.
-""") == 13);
+""") == 43);
     }
 
     public int Solve(string input)
     {
         var rows = input.Split(Environment.NewLine);
         var result = 0;
-        for (int y = 0; y < rows.Length; y++)
-        {
-            for (int x = 0; x < rows[y].Length; x++)
-            {
-                const char rollOfPaper = '@';
-                if (rows[y][x] != rollOfPaper) continue;
+        var wasRemoved = false;
 
-                var adjacent = Adjacent(x, y);
-                var count = adjacent.Count(t => t == rollOfPaper);
-                if (count < 4)
-                    result++;
+        do
+        {
+            wasRemoved = false;
+            var newRows = rows.ToArray();
+
+            for (int y = 0; y < rows.Length; y++)
+            {
+                for (int x = 0; x < rows[y].Length; x++)
+                {
+                    const char rollOfPaper = '@';
+                    if (rows[y][x] != rollOfPaper) continue;
+
+                    var adjacent = Adjacent(x, y);
+                    var count = adjacent.Count(t => t == rollOfPaper);
+                    if (count < 4)
+                    {
+                        wasRemoved = true;
+                        result++;
+
+                        const char removed = 'x';
+
+                        newRows[y] = new StringBuilder(newRows[y].Length)
+                            .Append(newRows[y][..x])
+                            .Append(removed)
+                            .Append(newRows[y][(x + 1)..])
+                            .ToString();
+                    }
+                }
             }
-        }
+
+            rows = newRows;
+        } while (wasRemoved);
 
         return result;
 
@@ -57,7 +80,6 @@ internal class Solver
                     yield return rows[y][x];
                 }
             }
-
         }
     }
 }
