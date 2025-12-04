c3905bae21649e9c048afe3085b50c87cce17ccb
Jan Vargovský <Jan.Vargovsky@rws.com>
2025-12-04 10:58:45 +0100

    ✨ Add 2025 Day 4 Part 1

---
+++ 2025/Day04/Solver.cs
@@ -0,0 +1,63 @@
+namespace AdventOfCode.Year2025.Day04;
+
+internal class Solver
+{
+    public Solver()
+    {
+        Debug.Assert(Solve("""
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+""") == 13);
+    }
+
+    public int Solve(string input)
+    {
+        var rows = input.Split(Environment.NewLine);
+        var result = 0;
+        for (int y = 0; y < rows.Length; y++)
+        {
+            for (int x = 0; x < rows[y].Length; x++)
+            {
+                const char rollOfPaper = '@';
+                if (rows[y][x] != rollOfPaper) continue;
+
+                var adjacent = Adjacent(x, y);
+                var count = adjacent.Count(t => t == rollOfPaper);
+                if (count < 4)
+                    result++;
+            }
+        }
+
+        return result;
+
+        IEnumerable<char> Adjacent(int midX, int midY)
+        {
+            for (var yi = -1; yi <= 1; yi++)
+            {
+                var y = midY + yi;
+
+                if (y < 0 || y >= rows.Length) continue;
+
+                for (var xi = -1; xi <= 1; xi++)
+                {
+                    if (yi == 0 && xi == 0) continue;
+
+                    var x = midX + xi;
+
+                    if (x < 0 || x >= rows[y].Length) continue;
+
+                    yield return rows[y][x];
+                }
+            }
+
+        }
+    }
+}
